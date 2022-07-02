0c9501e8a4085d9ddfbd3599b7f2d09371a43e75
Lomzom <bach287@gmail.com>
2022-07-02 22:59:27 +0700

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Bách Nguyễn Viết on 02/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
