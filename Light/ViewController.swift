214242a0f1dac72202edf5076337216ec36028d9
Lomzom <bach287@gmail.com>
2022-07-03 00:18:48 +0700

    first commit

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = true
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateIU()
         // Do any additional setup after loading the view.
     }
-
+    fileprivate func updateIU() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateIU()
+        
+    }
+    
 
 }
 
